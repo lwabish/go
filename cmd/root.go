0befddb1ec25815743ffa36ab4d63b0b08b43b4e
lwabish <wubw@pku.edu.cn>
2022-08-12 14:36:46 +0800

    feat: add gen doc

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,15 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/lwabish/go-snippets/cmd/exp"
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 	"os"
 )
 
 var version = "v1.0.3"
+var genDoc = false
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -17,13 +20,16 @@ var rootCmd = &cobra.Command{
 contact: imwubowen@gmail.com`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("add -h to see help message")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
+	generateDoc()
 	if err != nil {
 		os.Exit(1)
 	}
@@ -40,4 +46,13 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&genDoc, "gen-doc", "g", false, "generate doc and exit")
+}
+
+func generateDoc() {
+	if genDoc {
+		fmt.Printf("generating doc tree")
+		_ = doc.GenMarkdownTree(rootCmd, "./docs/")
+		os.Exit(0)
+	}
 }
